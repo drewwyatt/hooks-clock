a6faea3b63ded694b3e85617ec0e2edea45a80cb
Drew Wyatt <drew.wyatt@onepeloton.com>
2019-04-07 17:15:13 -0400

    wrote tests

--- src/components/Clock/useTime.test.ts
+++ src/components/Clock/useTime.test.ts
@@ -1,11 +1,44 @@
 import { createContext } from 'react'
-import { cleanup, renderHook } from 'react-hooks-testing-library'
-import { DEFAULT_STATE, State } from './state'
-import { useTime as hook } from './useTime'
+import { act, cleanup, renderHook } from 'react-hooks-testing-library'
+import { DEFAULT_STATE, State, actions } from './state'
+import { useTime as hook, HourType, AmPm } from './useTime'
 
 /*********
- * Setup *
+ * Mocks *
  *********/
+
+jest.useFakeTimers()
+
+let CURR_HOURS = 0
+let CURR_MINUTES = 0
+let CURR_SECONDS = 0
+
+Date.prototype.getHours = jest.fn().mockReturnValue(CURR_HOURS)
+Date.prototype.getMinutes = jest.fn().mockReturnValue(CURR_MINUTES)
+Date.prototype.getSeconds = jest.fn().mockReturnValue(CURR_SECONDS)
+
+const advanceTimeBySeconds = (n: number) => {
+  CURR_SECONDS++
+  for (let i = n; i > 0; i--) {
+    CURR_SECONDS++
+    ;(Date.prototype.getSeconds as jest.Mock).mockReturnValue(CURR_SECONDS)
+    jest.advanceTimersByTime(1000)
+  }
+}
+
+type TimeEvent = Parameters<typeof actions.setHourType>[1]
+const getTimeEvent = (hourType: HourType, amPm: AmPm = AmPm.am): TimeEvent =>
+  ({
+    hours: CURR_HOURS,
+    minutes: CURR_MINUTES,
+    seconds: CURR_SECONDS,
+    amPm: AmPm.am,
+    hourType,
+  } as any)
+
+/****************
+ * Test Harness *
+ ****************/
 
 const makeContext = (state: State, dispatch: () => any = jest.fn()) =>
   createContext({ state, dispatch })
@@ -16,14 +49,43 @@ const useTime = (context: ReturnType<typeof makeContext>) =>
 /*********
  * Tests *
  *********/
+
 afterEach(cleanup)
 
 describe('useTime', () => {
-  it('does something', () => {
-    const {
-      result: { current },
-    } = useTime(makeContext(DEFAULT_STATE))
-    const [a] = current
-    expect(a).toEqual(DEFAULT_STATE)
+  let dispatch: jest.Mock<any>
+  let result: ReturnType<typeof useTime>['result']
+  beforeAll(() => {
+    dispatch = jest.fn()
+    const r = useTime(makeContext(DEFAULT_STATE, dispatch))
+    result = r.result
+  })
+
+  afterEach(() => {
+    dispatch.mockReset()
+  })
+
+  it('will initialize itself', () => {
+    expect(dispatch).toHaveBeenCalledTimes(1)
+  })
+
+  it('can return a state', () => {
+    const [state] = result.current
+    expect(state).toEqual(DEFAULT_STATE)
+  })
+
+  it('Dispatches an action each second', () => {
+    advanceTimeBySeconds(5)
+    expect(dispatch).toHaveBeenCalledTimes(5)
+  })
+
+  it('returns a callback to update hourType', () => {
+    const [, updateHourType] = result.current
+    act(() => {
+      updateHourType(HourType.twelve)
+    })
+    expect(dispatch).toHaveBeenCalledWith(
+      actions.setHourType(HourType.twelve, getTimeEvent(HourType.twelve)),
+    )
   })
 })
