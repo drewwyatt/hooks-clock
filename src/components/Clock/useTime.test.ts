339d7e4594b97889820ea80a9d37f032f2f3514e
Drew Wyatt <drew.wyatt@onepeloton.com>
2019-04-07 16:09:56 -0400

    cleanup test harness

--- src/components/Clock/useTime.test.ts
+++ src/components/Clock/useTime.test.ts
@@ -1,24 +1,28 @@
 import { createContext } from 'react'
 import { cleanup, renderHook } from 'react-hooks-testing-library'
-import { DEFAULT_STATE } from './state'
+import { DEFAULT_STATE, State } from './state'
+import { useTime as hook } from './useTime'
 
 /*********
- * Mocks *
+ * Setup *
  *********/
 
-const MyContext = createContext({ state: DEFAULT_STATE, dispatch: jest.fn() })
+const makeContext = (state: State, dispatch: () => any = jest.fn()) =>
+  createContext({ state, dispatch })
+
+const useTime = (context: ReturnType<typeof makeContext>) =>
+  renderHook(() => hook(context))
 
 /*********
  * Tests *
  *********/
-import { useTime } from './useTime'
 afterEach(cleanup)
 
 describe('useTime', () => {
   it('does something', () => {
     const {
       result: { current },
-    } = renderHook(() => useTime(MyContext))
+    } = useTime(makeContext(DEFAULT_STATE))
     const [a] = current
     expect(a).toEqual(DEFAULT_STATE)
   })
