ef740c10dccf6ffbea947fa2a7484d4c451ce76f
Drew Wyatt <drew.wyatt@onepeloton.com>
2019-04-07 12:33:34 -0400

    format with prettier

--- src/components/ClockClass.tsx
+++ src/components/ClockClass.tsx
@@ -1,27 +1,27 @@
-import * as React from "react";
-import { Time, TimeEvent, AmPm, HourType } from "../lib";
+import * as React from 'react'
+import { Time, TimeEvent, AmPm, HourType } from '../lib'
 
 type State = {
-  hours: number;
-  minutes: number;
-  seconds: number;
-  amPm: AmPm;
-  hourType: HourType;
-};
+  hours: number
+  minutes: number
+  seconds: number
+  amPm: AmPm
+  hourType: HourType
+}
 
 export class Clock extends React.Component<{}, State> {
-  private time = new Time();
+  private time = new Time()
   public state = {
     hours: 0,
     minutes: 0,
     seconds: 0,
     amPm: AmPm.na,
-    hourType: HourType.twentyFour
-  };
+    hourType: HourType.twentyFour,
+  }
 
   componentDidMount() {
-    this.time.on("seconds", this.update.bind(this));
-    this.time.on("hourType", this.update.bind(this));
+    this.time.on('seconds', this.update.bind(this))
+    this.time.on('hourType', this.update.bind(this))
   }
 
   render() {
@@ -31,11 +31,11 @@ export class Clock extends React.Component<{}, State> {
         <h1>{this.getFormattedTime()}</h1>
         {this.hourTypeSelect()}
       </fieldset>
-    );
+    )
   }
 
   private hourTypeSelect = (): JSX.Element[] =>
-    [HourType.twelve, HourType.twentyFour].map(this.hourTypeToRadio.bind(this));
+    [HourType.twelve, HourType.twentyFour].map(this.hourTypeToRadio.bind(this))
 
   private hourTypeToRadio = (hourType: HourType): JSX.Element => (
     <label>
@@ -49,31 +49,31 @@ export class Clock extends React.Component<{}, State> {
       />
       {hourType}
     </label>
-  );
+  )
 
   private handleHourClick = ({ target: { value } }: any) => {
-    this.time.setHourType(value);
-  };
+    this.time.setHourType(value)
+  }
 
   private isSelectedHourType = (hourType: HourType): boolean =>
-    hourType === this.state.hourType;
+    hourType === this.state.hourType
 
   private setHourType = (hourType: HourType) => {
-    this.time.setHourType(hourType);
-  };
+    this.time.setHourType(hourType)
+  }
 
   private getFormattedTime = () => {
-    const { hours, minutes, seconds, amPm } = this.state;
-    const numbers = [hours, minutes, seconds].map(this.pad0).join(":");
-    return `${numbers} ${amPm}`;
-  };
+    const { hours, minutes, seconds, amPm } = this.state
+    const numbers = [hours, minutes, seconds].map(this.pad0).join(':')
+    return `${numbers} ${amPm}`
+  }
 
   private pad0 = (n: number): string => {
-    const str = n.toString();
-    return str.length > 1 ? str : "0" + str;
-  };
+    const str = n.toString()
+    return str.length > 1 ? str : '0' + str
+  }
 
   private update = (_: unknown, time: TimeEvent): void => {
-    this.setState(time);
-  };
+    this.setState(time)
+  }
 }
--- src/lib/time.ts
+++ src/lib/time.ts
@@ -1,115 +1,115 @@
 export enum HourType {
-  twelve = "12 hour",
-  twentyFour = "24 hour"
+  twelve = '12 hour',
+  twentyFour = '24 hour',
 }
 
 const isTwelveHour = (hourType: HourType): hourType is HourType.twelve =>
-  hourType === HourType.twelve;
+  hourType === HourType.twelve
 
 export enum AmPm {
-  am = "am",
-  pm = "pm",
-  na = ""
+  am = 'am',
+  pm = 'pm',
+  na = '',
 }
 
 type BaseTimeEvent = {
-  hours: number;
-  minutes: number;
-  seconds: number;
-};
+  hours: number
+  minutes: number
+  seconds: number
+}
 
 export interface TwelveHourTimeEvent extends BaseTimeEvent {
-  hourType: HourType.twelve;
-  amPm: AmPm.am | AmPm.pm;
+  hourType: HourType.twelve
+  amPm: AmPm.am | AmPm.pm
 }
 
 export interface TwentyFourHourTimeEvent extends BaseTimeEvent {
-  hourType: HourType.twelve;
-  amPm: AmPm.na;
+  hourType: HourType.twelve
+  amPm: AmPm.na
 }
 
-const INTERVAL_KEY = "DREWS_INTERVAL_KEY";
+const INTERVAL_KEY = 'DREWS_INTERVAL_KEY'
 const setThatOneInterval = (n: number) => {
   if ((window as any)[INTERVAL_KEY]) {
-    console.log("clearing...");
-    clearInterval((window as any)[INTERVAL_KEY]);
+    console.log('clearing...')
+    clearInterval((window as any)[INTERVAL_KEY])
   }
 
-  (window as any)[INTERVAL_KEY] = n;
-};
-
-export type TimeEvent = TwelveHourTimeEvent | TwentyFourHourTimeEvent;
-
-interface EventParams {
-  hours: number;
-  minutes: number;
-  seconds: number;
-
-  hourType: HourType;
+  ;(window as any)[INTERVAL_KEY] = n
 }
 
-type EventCallback<T> = (val: T, timeEvent: TimeEvent) => void;
+export type TimeEvent = TwelveHourTimeEvent | TwentyFourHourTimeEvent
 
-type CallbackSet = Record<keyof EventParams, EventCallback<any>>;
+interface EventParams {
+  hours: number
+  minutes: number
+  seconds: number
+
+  hourType: HourType
+}
+
+type EventCallback<T> = (val: T, timeEvent: TimeEvent) => void
+
+type CallbackSet = Record<keyof EventParams, EventCallback<any>>
 
 export class Time {
-  private _hours: number;
-  private _minutes: number;
-  private _seconds: number;
-  private _hourType: HourType = HourType.twentyFour;
-  private callbacks: Partial<CallbackSet> = {};
+  private _hours: number
+  private _minutes: number
+  private _seconds: number
+  private _hourType: HourType = HourType.twentyFour
+  private callbacks: Partial<CallbackSet> = {}
 
   constructor() {
-    const date = new Date();
-    this._hours = date.getHours();
-    this._minutes = date.getMinutes();
-    this._seconds = date.getSeconds();
+    const date = new Date()
+    this._hours = date.getHours()
+    this._minutes = date.getMinutes()
+    this._seconds = date.getSeconds()
 
-    setThatOneInterval(setInterval(this._handleInterval.bind(this), 1000));
+    setThatOneInterval(setInterval(this._handleInterval.bind(this), 1000))
   }
 
   public setHourType(value: HourType) {
-    this.set("hourType", value);
+    this.set('hourType', value)
   }
 
   public getHourType() {
-    return this._hourType;
+    return this._hourType
   }
 
   public getHours(): number {
     return isTwelveHour(this._hourType) && this._hours > 12
       ? this._hours - 12
-      : this._hours;
+      : this._hours
   }
 
   public getMinutes(): number {
-    return this._minutes;
+    return this._minutes
   }
 
   public getSeconds(): number {
-    return this._seconds;
+    return this._seconds
   }
 
   public getAmPm(): AmPm {
     if (isTwelveHour(this.getHourType())) {
-      return this._hours > 11 ? AmPm.pm : AmPm.am;
+      return this._hours > 11 ? AmPm.pm : AmPm.am
     }
 
-    return AmPm.na;
+    return AmPm.na
   }
 
   public on<TEvent extends keyof EventParams>(
     event: TEvent,
-    callback: EventCallback<EventParams[TEvent]>
+    callback: EventCallback<EventParams[TEvent]>,
   ): void {
-    this.callbacks[event] = callback;
+    this.callbacks[event] = callback
   }
 
   private _handleInterval() {
-    const date = new Date();
-    this.set("hours", date.getHours());
-    this.set("minutes", date.getMinutes());
-    this.set("seconds", date.getSeconds());
+    const date = new Date()
+    this.set('hours', date.getHours())
+    this.set('minutes', date.getMinutes())
+    this.set('seconds', date.getSeconds())
   }
 
   private getTimeEvent(): TimeEvent {
@@ -118,31 +118,31 @@ export class Time {
       minutes: this.getMinutes(),
       seconds: this.getSeconds(),
       hourType: this.getHourType(),
-      amPm: this.getAmPm()
-    } as TimeEvent;
+      amPm: this.getAmPm(),
+    } as TimeEvent
   }
 
   private set(
-    key: "hours" | "minutes" | "seconds" | "hourType",
-    value: number | HourType
+    key: 'hours' | 'minutes' | 'seconds' | 'hourType',
+    value: number | HourType,
   ) {
-    if (key === "hourType" && this._hourType !== value) {
-      this._hourType = value as HourType;
-      this.triggerCallbackIfExists(key, value as any);
-    } else if (["hours", "minutes", "seconds"].indexOf(key) > -1) {
-      if (this["_" + key]! !== (value as number)) {
-        this["_" + key] = value as number;
-        this.triggerCallbackIfExists(key, value as any);
+    if (key === 'hourType' && this._hourType !== value) {
+      this._hourType = value as HourType
+      this.triggerCallbackIfExists(key, value as any)
+    } else if (['hours', 'minutes', 'seconds'].indexOf(key) > -1) {
+      if (this['_' + key]! !== (value as number)) {
+        this['_' + key] = value as number
+        this.triggerCallbackIfExists(key, value as any)
       }
     }
   }
 
   private triggerCallbackIfExists<TEvent extends keyof EventParams>(
     event: TEvent,
-    params: EventParams[TEvent]
+    params: EventParams[TEvent],
   ) {
     if (event in this.callbacks) {
-      this.callbacks[event](params, this.getTimeEvent());
+      this.callbacks[event](params, this.getTimeEvent())
     }
   }
 }
